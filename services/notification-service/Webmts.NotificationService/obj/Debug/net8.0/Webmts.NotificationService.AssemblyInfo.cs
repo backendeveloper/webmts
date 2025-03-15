07ff0c73b6193280b65315c22abe1e7c00838ffc
Kaan Uygur <previousdeveloper+github@gmail.com>
2025-03-16 01:17:40 +0300

    added gitignore

--- services/notification-service/Webmts.NotificationService/obj/Debug/net8.0/Webmts.NotificationService.AssemblyInfo.cs
+++ services/notification-service/Webmts.NotificationService/obj/Debug/net8.0/Webmts.NotificationService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Webmts.NotificationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7061094a4725030043986ab70525ec914e421401")]
 [assembly: System.Reflection.AssemblyProductAttribute("Webmts.NotificationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Webmts.NotificationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
